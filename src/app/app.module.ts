95a5c46191d8be3522de13f2e10d7ca136431e0d
Juan Carlos Avila Meza <avilameza@gmail.com>
2025-02-23 21:46:31 -0500

    ajustes en los archivos del app-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Pages/Front/Components/navbar/navbar.component';
+import { FooterComponent } from './Pages/Front/Components/footer/footer.component';
+import { ContactbarComponent } from './Pages/Front/Components/contactbar/contactbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    ContactbarComponent
   ],
   imports: [
     BrowserModule,
