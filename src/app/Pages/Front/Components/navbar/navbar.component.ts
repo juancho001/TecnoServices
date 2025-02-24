7cca8efda34a4d189472fd79f990240ff64e67c5
Juan Carlos Avila Meza <avilameza@gmail.com>
2025-02-24 09:13:16 -0500

    Creación del NavBar

---
+++ src/app/Pages/Front/Components/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pages-front-navbar',
+  standalone: false,
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+  title = 'TecnoServices C.A';
+}
