3d54a9c73a3a5e976dbdf3c714707f81364d4d49
Juan Carlos Avila Meza <avilameza@gmail.com>
2025-02-25 19:13:58 -0500

    creación del contactbar

---
+++ src/app/Pages/Front/Components/contactbar/contactbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pages-front-contactbar',
+  standalone: false,
+  templateUrl: './contactbar.component.html',
+  styleUrl: './contactbar.component.css'
+})
+export class ContactbarComponent {
+
+}
